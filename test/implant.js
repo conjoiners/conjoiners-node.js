1538ed002cd794d4c762b4163e38bc6e6f9d26cc
Ben Ripkens <bripkens.dev@gmail.com>
2013-04-06 10:14:53 +0200

    implent Test property val nutzt nicht conjoiners.

--- test/implant.js
+++ test/implant.js
@@ -5,7 +5,9 @@ var conjoiners = require('../lib/conjoiners');
 exports['values should be accessible on implanted obj'] = function(test) {
     test.expect(1);
 
-    var obj = {};
+    var obj = {
+        val: null
+    };
 
     conjoiners.implant(obj, 'test/conf.json', 'test_implant').then(function() {
         obj.val = 'implant_value';
