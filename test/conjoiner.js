ed734422d13c23cf775049bff2bf34a474b1f842
Ben Ripkens <bripkens.dev@gmail.com>
2013-04-06 09:04:39 +0200

    Added logging, linted the refactored conjoiners version and fixed the first test.

--- test/conjoiner.js
+++ test/conjoiner.js
@@ -1,24 +1,29 @@
+'use strict';
+
 var conjoiners = require('../lib/conjoiners');
 
-var cj1 = {};
-var cj2 = {};
-conjoiners.implant(cj1, 'test/conf.json', 'test');
-conjoiners.implant(cj2, 'test/conf.json', 'test2');
-
 exports['simple inter-process communication'] = function(test) {
-    'use strict';
-
     test.expect(1);
-    var firstSleep = function() {
-        cj1.val = 'test_value';
-        setTimeout(secondSleep, 1000);
-    };
 
-    var secondSleep = function() {
-        test.equal(cj2.val, 'test_value');
-        test.done();
+    var cj1 = {
+        val: null
     };
+    var cj2 = {};
 
-    setTimeout(firstSleep, 1500);
+    conjoiners.implant(cj1, 'test/conf.json', 'test').then(function() {
+        return conjoiners.implant(cj2, 'test/conf.json', 'test2');
+    }).then(function () {
+        var firstSleep = function() {
+            cj1.val = 'test_value';
+            setTimeout(secondSleep, 1000);
+        };
+
+        var secondSleep = function() {
+            test.equal(cj2.val, 'test_value');
+            test.done();
+        };
+
+        setTimeout(firstSleep, 1500);
+    }).done();
 };
 
