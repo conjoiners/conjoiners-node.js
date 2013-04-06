80b70b8a6a2af0e93f7ebed0628d6007d72c3c60
Ben Ripkens <bripkens.dev@gmail.com>
2013-04-06 09:39:33 +0200

    Fixed both tests and ensured that the hits example is working fine.

--- test/implant.js
+++ test/implant.js
@@ -1,19 +1,15 @@
+'use strict';
+
 var conjoiners = require('../lib/conjoiners');
 
 exports['values should be accessible on implanted obj'] = function(test) {
-    'use strict';
-
     test.expect(1);
 
     var obj = {};
-    conjoiners.implant(obj, 'test/conf.json', 'test_implant', function() {
+
+    conjoiners.implant(obj, 'test/conf.json', 'test_implant').then(function() {
         obj.val = 'implant_value';
-
-        var afterSleep = function() {
-            test.equal(obj.val, 'implant_value');
-            test.done();
-        };
-
-        setTimeout(afterSleep, 100);
+        test.equal(obj.val, 'implant_value');
+        test.done();
     });
 };
